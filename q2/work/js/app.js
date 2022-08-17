3ba3512e5505cb9f86b86251452819f108f52428
ayana <ayana.s.prose@gmail.com>
2022-08-17 22:21:38 +0900

    submission

--- q2/work/js/app.js
+++ q2/work/js/app.js
@@ -1,11 +1,14 @@
 $(function () {
-  $('.modal_open_button').on('click',function () {
   //OPENボタンをクリックでイベント発生
-    $('.modal_bg').fadeIn(),//グレーの背景をフェードインで表示
-    $('.modal_win').show();//display:noneになっていたモーダルを表示
+  $('.modal_open_button').on('click',function () {
+    //グレーの背景をフェードインで表示
+    $('.modal_bg').fadeIn(),
+    //display:noneになっていたモーダルを表示
+    $('.modal_win').show();
   });
-  $('.modal_close_button').on('click',function () {
   //クローズボタンを押すとイベント発生
-    $('.modal_bg, .modal_win').fadeOut();//背景色とモーダルをフェードアウト
+  $('.modal_close_button').on('click',function () {
+    //背景色とモーダルをフェードアウト
+    $('.modal_bg, .modal_win').fadeOut();
   });
 });
