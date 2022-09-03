e0bda1a05a0e5fc8e3e74f30b821c5d679d85beb
ayana <ayana.s.prose@gmail.com>
2022-09-03 22:15:08 +0900

    submission

--- q6/work/js/app.js
+++ q6/work/js/app.js
@@ -1,12 +1,21 @@
 $(function(){
+  //セレクトボックスの変更でイベント発生
 	$('.select-box').on('change', function() {
+    //selectedの変数に選択したタブのvalueの値を拾い代入
     const selected = $(this).val();
+    //リスト内を下記のif処理でループさせる
     $('.food-list li').each(function(){
+      //リスト内のデータと変数の値が一致した時の処理
       if ($(this).data('category-type') === selected) {
+        //そのリストを表示する
         $(this).show();
+        //変数の値が'all'だった場合の処理
 			} else if(selected === 'all'){
+        //リストの全てを表示する
 				$(this).show();
+        //上記以外の処理
       } else {
+        //表示を隠す
         $(this).hide();
       }
     });
@@ -14,6 +23,6 @@ $(function(){
 });
 
 
-/* 8行目効かなかったもの
-$('.select-box')[0].reset();
+/*鈴木メモ
+・reset()はformのinputのname属性に対して有効なため、今回の実装ではエラーになる。
 */
