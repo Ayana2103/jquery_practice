37adb91c3250b010172d25359432a992cf71fb83
ayana <ayana.s.prose@gmail.com>
2022-09-07 23:21:23 +0900

    submission

--- q8/work/js/app.js
+++ q8/work/js/app.js
@@ -1,3 +1,36 @@
+//変数settingsに設定情報などを格納
+const settings = {
+  //実⾏するURL(エンドポイント)
+  "url": `https://ci.nii.ac.jp/books/opensearch/search?title=${searchWord}&format=json&p=${pageCount}&count=20`,
+  //サーバーに送るメソッド
+  "method": "GET",
+}
+//Ajaxの実⾏
+$.ajax(settings).done(function (response) {
+  //通信成功した時の処理。引数responseが通信した結果を受け取っている
+  const result = response['@graph'];
+  displayResult(result)
+//通信に失敗した時の処理。errにサーバーから送られてきたエラー内容を受け取っている
+}).fail(function (err) {
+  displayError(err)
+});
+
+
+/*　リセット　最後　未
+$('.reset-btn').on('click', function(){
+  $('.lists').empty();
+});
+*/
+
+
+
+
+
+
+
+
+
+
 // API
 // const settings = {
 //   "url": `https://ci.nii.ac.jp/books/opensearch/search?title=${searchWord}&format=json&p=${pageCount}&count=20`,
