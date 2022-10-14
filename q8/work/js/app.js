002250b9f162879d816b52be7a80be9ff691f2fd
ayana <ayana.s.prose@gmail.com>
2022-10-14 21:17:13 +0900

    submission

--- q8/work/js/app.js
+++ q8/work/js/app.js
@@ -13,22 +13,22 @@ $(function () {
 
 
 
-//変数settingsに設定情報などを格納
-const settings = {
-  //実⾏するURL(エンドポイント)
-  "url": `https://ci.nii.ac.jp/books/opensearch/search?title=${searchWord}&format=json&p=${pageCount}&count=20`,
-  //サーバーに送るメソッド
-  "method": "GET",
-}
-//Ajaxの実⾏
-$.ajax(settings).done(function (response) {
-  //通信成功した時の処理。引数responseが通信した結果を受け取っている
-  const result = response['@graph'];
-  displayResult(result)
-//通信に失敗した時の処理。errにサーバーから送られてきたエラー内容を受け取っている
-}).fail(function (err) {
-  displayError(err)
-});
+  //変数settingsに設定情報などを格納
+  const settings = {
+    //実⾏するURL(エンドポイント)
+    "url": `https://ci.nii.ac.jp/books/opensearch/search?title=${searchWord}&format=json&p=${pageCount}&count=20`,
+    //サーバーに送るメソッド
+    "method": "GET",
+  }
+  //Ajaxの実⾏
+  $.ajax(settings).done(function (response) {
+    //通信成功した時の処理。引数responseが通信した結果を受け取っている
+    const result = response['@graph'];
+    displayResult(result)
+  //通信に失敗した時の処理。errにサーバーから送られてきたエラー内容を受け取っている
+  }).fail(function (err) {
+    displayError(err)
+  });
 });
 
 const displayResult = any;
