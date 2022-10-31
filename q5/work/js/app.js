e7c137da89f23e0e0a42f468fcfc59badb701dc2
ayana <ayana.s.prose@gmail.com>
2022-10-31 23:39:03 +0900

    submission

--- q5/work/js/app.js
+++ q5/work/js/app.js
@@ -2,11 +2,11 @@ $(function () {
   //要素のクリックでイベント発生
   $('ul.dropdwn li').hover(function(){
     //display:noneになっていた要素がスライドダウンで表示される
-    $('.dropdwn_menu:not(:animated)', this).slideDown();
+    $('.dropdwn_menu:not(:animated)', this).stop().slideDown();
   //カーソル外れた時の処理
   },function(){
       //スライドアップでdisplay:noneが適用され隠れる
-      $('.dropdwn_menu', this).slideUp();
+      $('.dropdwn_menu', this).stop().slideUp();
   });
 });
 
@@ -19,4 +19,5 @@ $(function () {
 ・“〇〇:not(:animated)”　→アニメーションの途中にアニメーションを実行しないでという意味
   これがないと、例えばhoverではマウスを置いた回数分アニメーションが実行されてしまう。
 ・.on()や.bind()ではhoverイベントは使えない
+・stop()はアニメーション停止メソッド（アニメーション中の動きも止める）
 */
