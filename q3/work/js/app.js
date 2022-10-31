f998c7184154070f619144d91585b8d45bbe8449
ayana <ayana.s.prose@gmail.com>
2022-10-31 23:13:21 +0900

    submission

--- q3/work/js/app.js
+++ q3/work/js/app.js
@@ -8,4 +8,14 @@ $(function () {
     //指定した背景要素に対してclass属性の表示・非表示をフェードイン・アウトで繰り返す
     $('.drawer_bg').fadeToggle('left');
   });
+
+  //グレー背景クリックでイベント発生
+  $('.drawer_bg').on('click', function(){
+    //指定した要素の背景色を非表示にする
+    $(this).hide();
+    //下記要素のactiveクラスを削除
+    $('.drawer_button').removeClass('active');
+    //下記要素のopenクラスを削除
+    $('.drawer_nav_wrapper, .drawer_nav').removeClass('open');
+  });
 });
