b2ce8165300c58f0e33cebcd03097e24de70b364
ayana <ayana.s.prose@gmail.com>
2022-11-01 00:59:46 +0900

    submission

--- q6/work/js/app.js
+++ q6/work/js/app.js
@@ -3,6 +3,38 @@ $(function(){
 	$('.select-box').on('change', function() {
     //selectedの変数に選択したタブのvalueの値を拾い代入
     const selected = $(this).val();
+    //変数の値が'all'だった場合
+    if(selected === 'all'){
+      //リストの全てを表示する
+      $('.food-list li').show()
+    };
+    //リスト内を下記のif処理でループさせる
+    $('.food-list li').each(function(){
+      //リスト内のデータと変数の値が一致した時の処理
+      if ($(this).data('category-type') === selected) {
+        //そのリストを表示する
+        $(this).show();
+        //上記以外の場合
+      } else {
+        //表示を隠す
+        $(this).hide();
+      }
+    });
+
+  });
+});
+
+
+/*鈴木メモ
+・reset()はformのinputのname属性に対して有効なため、今回の実装ではエラーになる。
+*/
+
+/* 前回の実装　→　eachにallの実装を含まないよう訂正が必要
+$(function(){
+  //セレクトボックスの変更でイベント発生
+	$('.select-box').on('change', function() {
+    //selectedの変数に選択したタブのvalueの値を拾い代入
+    const selected = $(this).val();
     //リスト内を下記のif処理でループさせる
     $('.food-list li').each(function(){
       //リスト内のデータと変数の値が一致した時の処理
@@ -21,8 +53,4 @@ $(function(){
     });
   });
 });
-
-
-/*鈴木メモ
-・reset()はformのinputのname属性に対して有効なため、今回の実装ではエラーになる。
 */
